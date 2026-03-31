c1fa55f2f483516c5637c2776bf5eeeac506b1fa
Mark Arseneault <mark@arsnl.dev>
2026-03-31 14:30:19 -0400

    chore: use tsdown in oxlint package

---
+++ packages/oxlint/tsdown.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig({
+  entry: ["src/**/*.ts", "!src/**/*.test.ts"],
+  // To make sure the output files have the .js and .d.ts extension.
+  fixedExtension: false,
+  deps: {
+    skipNodeModulesBundle: true,
+  },
+  attw: {
+    profile: "esm-only",
+    level: "error",
+  },
+  publint: {
+    level: "error",
+  },
+});
