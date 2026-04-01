175d5f931de8292d493e1939d21c96655bda31f0
Mark Arseneault <mark@arsnl.dev>
2026-04-01 15:22:28 -0400

    chore: add new linting rules for oxc configuration

--- packages/ox/src/lint/configs/core/rules/oxc.ts
+++ packages/ox/src/lint/configs/core/rules/oxc.ts
@@ -1,7 +1,9 @@
 import type { DummyRuleMap, OxlintConfig } from "oxlint";
 
 export const rules = {
+  "oxc/approx-constant": ["error"],
   "oxc/bad-array-method-on-arguments": ["error"],
+  "oxc/bad-bitwise-operator": ["error"],
   "oxc/bad-char-at-comparison": ["error"],
   "oxc/bad-comparison-sequence": ["error"],
   "oxc/bad-min-max-func": ["error"],
@@ -10,7 +12,9 @@ export const rules = {
   "oxc/const-comparisons": ["error"],
   "oxc/double-comparisons": ["error"],
   "oxc/erasing-op": ["error"],
+  "oxc/misrefactored-assign-op": ["error"],
   "oxc/missing-throw": ["error"],
+  "oxc/no-const-enum": ["error"],
   "oxc/number-arg-out-of-range": ["error"],
   "oxc/only-used-in-recursion": ["error"],
   "oxc/uninvoked-array-callback": ["error"],
