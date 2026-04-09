ad00a6b4982a92d8452a9ac801424aa679d1dca0
Mark Arseneault <mark@arsnl.dev>
2026-04-09 11:50:17 -0400

    feat: add utility function to retrieve combined rules metadata from built-in and JavaScript rules

---
+++ packages/ox/src/lint/utils/get-rules-meta.ts
@@ -0,0 +1,12 @@
+import { getBuiltInRulesMeta } from "./get-built-in-rules-meta.ts";
+import { getJsRulesMeta } from "./get-js-rules-meta.ts";
+
+/**
+ * Get the rules metadata.
+ *
+ * This function is a wrapper around `getBuiltInRulesMeta` and `getJsRulesMeta` to get all the rules
+ * metadata.
+ *
+ * @returns The rules metadata.
+ */
+export const getRulesMeta = () => [...getBuiltInRulesMeta(), ...getJsRulesMeta()];
